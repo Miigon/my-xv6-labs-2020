3528717954bd1ed938963034c3e5ed34e8ead079
Miigon <miigon.pg@gmail.com>
2021-10-06 02:53:46 +0800

    exp1

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for thread context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
-              
+extern void thread_switch(struct context* old, struct context* new);
+
 void 
 thread_init(void)
 {
@@ -59,10 +78,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(&t->ctx, &next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -76,7 +92,11 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;       // return address
+  // thread_switch will return to ra, thus running the thread
+  t->ctx.sp = (uint64)&t->stack + (STACK_SIZE - 1);  // stack pointer
+  // set thread's stack pointer to it's own stack, notice that stack grows from high address to low address
+  // so it starts from the highest address of t->stack;
 }
 
 void 
